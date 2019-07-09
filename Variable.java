4866d07fcd7a8f82acb7830a1229110afc43554c
DonAntonioBiker <48891871+DonAntonioBiker@users.noreply.github.com>
2019-07-09 11:08:18 +0300

    Add files via upload
    lesson 1

---
+++ Variable.java
@@ -0,0 +1,22 @@
+package com.qwertovsky.Variable;
+public class Variable {
+	public static void main(String[] args) {
+		byte cpuamount = 4;
+		short ramgb = 16;
+		int ramamount = 4;
+		long hddcapacity = 500105736192;
+		float frequency = 2.6;
+		double price = 1200.30;
+		char cpumaker = 'i';
+		boolean good = true;
+
+		System.out.println(cpuamount);
+		System.out.println(ramgb);
+		System.out.println(ramamount);
+		System.out.println(fanspeed);
+		System.out.println(frquency);
+		System.out.println(price);
+		System.out.println(cpumaker);
+		System.out.println(good);
+	}
+}
