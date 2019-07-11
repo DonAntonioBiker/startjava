052c095d0b5d8dfe622ed48cdf6d99867b9dbf72
DonAntonioBiker <48891871+DonAntonioBiker@users.noreply.github.com>
2019-07-11 17:56:49 +0300

    fix Variable.java

--- Variable.java
+++ Variable.java
@@ -1,22 +1,22 @@
-package com.qwertovsky.Variable;
+package lesson1.Variable;
 public class Variable {
 	public static void main(String[] args) {
-		byte cpuamount = 4;
-		short ramgb = 16;
-		int ramamount = 4;
-		long hddcapacity = 500105736192;
-		float frequency = 2.6;
+		byte CpuAmount = 4;
+		short RamGb = 16;
+		int RamAmount = 4;
+		long HddCapacity = 500105736192L;
+		float frequency = 2.60f;
 		double price = 1200.30;
-		char cpumaker = 'i';
+		char CpuMaker = 'i';
 		boolean good = true;
 
-		System.out.println(cpuamount);
-		System.out.println(ramgb);
-		System.out.println(ramamount);
-		System.out.println(fanspeed);
-		System.out.println(frquency);
+		System.out.println(CpuAmount);
+		System.out.println(RamGb);
+		System.out.println(RamAmount);
+		System.out.println(HddCapacity);
+		System.out.println(frequency);
 		System.out.println(price);
-		System.out.println(cpumaker);
+		System.out.println(CpuMaker);
 		System.out.println(good);
 	}
 }
