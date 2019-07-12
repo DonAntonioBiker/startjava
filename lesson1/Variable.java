d6c83c50ab4fabc6d59bcc10148a67797a541008
DonAntonioBiker <vakulenko.anv@gmail.com>
2019-07-12 12:58:13 +0300

    Создание директории lesson1 с файлом Variable.java

---
+++ lesson1/Variable.java
@@ -0,0 +1,22 @@
+package lesson1.Variable;
+public class Variable {
+	public static void main(String[] args) {
+		byte CpuAmount = 4;
+		short RamGb = 16;
+		int RamAmount = 4;
+		long HddCapacity = 500105736192L;
+		float frequency = 2.60f;
+		double price = 1200.30;
+		char CpuMaker = 'i';
+		boolean good = true;
+
+		System.out.println(CpuAmount);
+		System.out.println(RamGb);
+		System.out.println(RamAmount);
+		System.out.println(HddCapacity);
+		System.out.println(frequency);
+		System.out.println(price);
+		System.out.println(CpuMaker);
+		System.out.println(good);
+	}
+}
