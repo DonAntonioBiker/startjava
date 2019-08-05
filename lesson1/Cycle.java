fc193c1559544c5d087c07e79fc1c209fc4ae766
DonAntonioBiker <48891871+DonAntonioBiker@users.noreply.github.com>
2019-08-06 00:22:05 +0300

    Update Cycle.java
    bugs fixed

--- lesson1/Cycle.java
+++ lesson1/Cycle.java
@@ -1,23 +1,23 @@
 public class Cycle {
     public static void main(String[] args) {
-    	for (i=0; i<=20; i++) {
+    	for (i = 0; i <= 20; i++) {
     		System.out.println(i);
     	}
 
-    	int a = 6;
-    	while (a >= -6) {
+    	int counter = 6;
+    	while (counter >= -6) {
     		System.out.println(a);
-    		a-=2;
+    		counter -= 2;
     	}
     	
-    	int b = 10;
-    	int c = 0;
+    	int counter = 10;
+    	int sumOdd = 0;
     	do {
-            if (b%2 != 0) {
-                c = c + b;
+            if (b % 2 != 0) {
+                c += b;
             }
             b++;
-        } while(b!=20);
+        } while(b != 20);
         System.out.println(c);
     }
 }
