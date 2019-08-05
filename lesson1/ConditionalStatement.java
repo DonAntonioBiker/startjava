3e782e7379ab36876f16f9d702564c027ac16c33
DonAntonioBiker <48891871+DonAntonioBiker@users.noreply.github.com>
2019-08-06 00:17:11 +0300

    Update ConditionalStatement.java
    bugs fixed

--- lesson1/ConditionalStatement.java
+++ lesson1/ConditionalStatement.java
@@ -2,29 +2,27 @@ public class ConditionalStatement {
 
     public static void main(String[] args) {
     	int age;
-    	String sex;
-    	double height;
-    	String name;
-    	char nameFirstLetter;
-
     	if(age > 20) {
     		System.out.println("Ваш возраст больше 20 лет");
     	}
-
-    	if(sex == "male") {
+        
+        char sex;
+    	if(sex == 'm') {
     		System.out.println("Вы - мужчина");
     	}
 
-    	if(sex == "female") {
+    	if(sex == 'f') {
     		System.out.println("Вы - женщина");
     	}
-
+        
+        double height;
     	if(height < 1.80) {
     		System.out.println("Вы высокий");
     	} else {
     		System.out.println("Вы низкий");
     	}
-
+        
+        char nameFirstLetter;
     	if(nameFirstLetter == 'М') {
     		System.out.println("Ваше имя начинается на букву M");
     	} else if(nameFirstLetter == 'И') {
